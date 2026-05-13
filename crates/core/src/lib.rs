2c9a02121303277a54dc4c2c61f20d8423d33441
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-05-13 02:32:01 -0400

    feat: pack validator

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -3,11 +3,12 @@ use std::fs::{self};
 use std::io;
 use std::path::Path;
 
-fn validate_pack(dir: &Path) -> io::Result<bool> {
+pub fn validate_pack(dir: &Path) -> io::Result<bool> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
+
             if !path.is_dir() && path.file_name() == Some(OsStr::new("manifest.json")) {
                 return Ok(true);
             }
@@ -22,7 +23,7 @@ mod tests {
 
     #[test]
     fn test() {
-        let path = Path::new("../../../test/example_pack/behavior_packs/pack0");
+        let path = Path::new("../../test/example_pack/behavior_packs/pack0/");
         assert!(validate_pack(path).unwrap());
     }
 }
