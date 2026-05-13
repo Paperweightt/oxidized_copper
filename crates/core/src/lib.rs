cbb5738d9e1de47171645ba58543dac22e466203
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-05-13 15:54:59 -0400

    feat: find packs

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -2,6 +2,8 @@ use std::ffi::OsStr;
 use std::fs::{self};
 use std::io;
 use std::path::Path;
+use std::path::PathBuf;
+use walkdir::WalkDir;
 
 pub fn validate_pack(dir: &Path) -> io::Result<bool> {
     if dir.is_dir() {
@@ -9,7 +11,7 @@ pub fn validate_pack(dir: &Path) -> io::Result<bool> {
             let entry = entry?;
             let path = entry.path();
 
-            if !path.is_dir() && path.file_name() == Some(OsStr::new("manifest.json")) {
+            if !path.is_dir() && path.file_name().unwrap() == OsStr::new("manifest.json") {
                 return Ok(true);
             }
         }
@@ -17,13 +19,46 @@ pub fn validate_pack(dir: &Path) -> io::Result<bool> {
     Ok(false)
 }
 
+pub fn find_packs(dir: Vec<PathBuf>) -> io::Result<Vec<PathBuf>> {
+    let mut packs: Vec<PathBuf> = Vec::new();
+
+    for path in dir {
+        let walker = WalkDir::new(path).into_iter().filter_map(|e| e.ok());
+
+        for entry in walker {
+            let path = entry.path();
+
+            if validate_pack(path)? {
+                packs.push(path.to_path_buf());
+            }
+        }
+    }
+
+    return Ok(packs);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test() {
-        let path = Path::new("../../test/example_pack/behavior_packs/pack0/");
-        assert!(validate_pack(path).unwrap());
+    fn test_pack_validator() {
+        let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let new_path = path.join("../../test/example_pack/behavior_packs/pack0/");
+
+        assert!(validate_pack(new_path.as_path()).unwrap());
+    }
+    #[test]
+    fn test_find_packs() {
+        let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+
+        let paths: Vec<PathBuf> = vec![
+            path.join("../../test/example_pack/"),
+            path.join("../../test/example_pack/resource_packs/pack0/"),
+        ];
+
+        let result = find_packs(paths);
+
+        assert_eq!(result.unwrap().len(), 3);
     }
 }
