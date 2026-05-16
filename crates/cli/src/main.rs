84c6ec57d2e5fe8f1d4c73908d8eae65c3ac0cda
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-05-16 03:24:25 -0400

    feat: cli works

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,5 +1,39 @@
+use std::path::PathBuf;
+
+use bump::BumpVersion;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Bump {
+        #[arg(long)]
+        path: PathBuf,
+        r#type: BumpVersion,
+    },
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Bump {
+        #[arg(long)]
+        major: bool,
+    },
+}
+
 fn main() {
-    match command {
-        "bump" => ts_transpile::run(args),
-    }
+    let cli = Cli::parse();
+
+    match cli.command {
+        // Command::Transpile => ts_transpile::run(),
+        // Command::Minify => minify_js::run(),
+        Command::Bump { path, r#type } => bump::bump_pack(bump::BumpArgs { path, r#type }),
+        // Command::Pack => packager::run(),
+        // Command::Validate => json_schema::run(),
+    };
 }
