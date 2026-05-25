79fbda60bed178ae3b40b741fda4ab46e82491e3
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-05-24 23:16:00 -0400

    feat: link junction

--- crates/link/src/lib.rs
+++ crates/link/src/lib.rs
@@ -1,13 +1,17 @@
+use std::fs;
 use std::path::Path;
 
 pub fn link(input: &Path, output: &Path) {
     if let Err(e) = junction::create(input, output) {
-        println!("[mcbe] link failed to create {e}")
+        panic!("[mcbe] Link failed to create: {e}")
     }
 }
 
 pub fn unlink(input: &Path) {
     if let Err(e) = junction::delete(input) {
-        println!("[mcbe] link failed to delete {e}")
+        panic!("[mcbe] Link failed to delete: {e}")
+    }
+    if let Err(e) = fs::remove_dir(input) {
+        panic!("[mcbe] Folder failed to delete: {e}")
     }
 }
